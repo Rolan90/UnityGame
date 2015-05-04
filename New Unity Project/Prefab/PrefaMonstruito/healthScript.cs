78e05cac8c9615c2c01b5c61f85a6157ba99457f
javierl-vr <javierlvrjavo@gmail.com>
2015-05-04 19:45:56 +0200

    Monstruito Prefabs

---
+++ New Unity Project/Prefab/PrefaMonstruito/healthScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthScript : MonoBehaviour {
+
+
+	public float health;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (health < 0f) 
+		{
+			enmeyIAScript.isPlayerAlive = false;
+			Destroy (gameObject);
+		}
+
+	
+	}
+}
